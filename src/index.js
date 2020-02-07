1782a65b9e2ddd06c08811058f130d9eb464fc39
Evgeny Ber <fiend.jackson@gmail.com>
2020-02-07 21:06:56 +0300

    выполнил начальную домашку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {App} from "./components/app.jsx";
+
+const placesCount = 132;
+
+ReactDOM.render(
+    <App places={placesCount} />,
+    document.getElementById(`root`)
+);
