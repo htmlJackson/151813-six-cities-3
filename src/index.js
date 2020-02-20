e0dabf1c1fae72f0030b403696729ca055e203d5
Evgeny Ber <fiend.jackson@gmail.com>
2020-02-20 17:40:14 +0300

    propTypes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {App} from "./components/app.jsx";
+import {App} from "./components/app/app.jsx";
 
 const placesCount = 132;
 
+const places = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Beautiful & luxurious apartment at great location`, `Wood and stone place`];
+
 ReactDOM.render(
-    <App places={placesCount} />,
+    <App placesCount={placesCount} places={places} />,
     document.getElementById(`root`)
 );
